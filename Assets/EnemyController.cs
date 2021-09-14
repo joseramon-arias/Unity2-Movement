3c2b3d8fc47a143105a6f30bbd5a17d4e339b310
Jose Ramon Arias <joseramon.arias28@gmail.com>
2021-09-14 12:05:17 -0700

    Push Animation Working (Needs a lot of tweaking)

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private float _attackDistance = 5f;
+    [SerializeField] private LayerMask _occlusionMask;
+    [SerializeField] private Vector3 _lookOffset = new Vector3(0f, 1.5f, 0f);
+
     private CharacterMovement _characterMovement;
     private GameObject _player;
 
@@ -17,6 +21,34 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        _characterMovement.MoveTo(_player.transform.position);
+        float targetDistance = Vector3.Distance(transform.position, _player.transform.position);
+        bool targetVisible = TestVisibility(_player.transform.position + _lookOffset);
+
+        if (targetDistance > _attackDistance || !targetVisible)
+        {
+            _characterMovement.MoveTo(_player.transform.position);
+        }
+        else
+        {
+            _characterMovement.StopMovement();
+            Vector3 targetDirection = (_player.transform.position - transform.position).normalized;
+            _characterMovement.SetLookDirection(targetDirection);
+        }
+    }
+
+    private bool TestVisibility(Vector3 targetPosition)
+    {
+        Vector3 start = transform.position + _lookOffset;
+        Vector3 end = targetPosition;
+
+        // linecast to test for occlusion, if we hit something, the target position isn't visible
+        if (Physics.Linecast(start, end, _occlusionMask))
+        {
+            Debug.DrawLine(start, end, Color.red);
+            return false;
+        }
+
+        Debug.DrawLine(start, end, Color.green);
+        return true;
     }
 }
